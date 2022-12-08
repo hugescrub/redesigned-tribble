495390d20852ba87b446432d0f2559edcc0d7ee5
hugescrub <hexi9001@gmail.com>
2022-12-08 18:25:27 +0300

    article controller

---
+++ src/main/java/net/newsportal/controller/ArticleController.java
@@ -0,0 +1,14 @@
+package net.newsportal.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/portal/news")
+public class ArticleController {
+
+}
---
+++ src/main/java/net/newsportal/models/dto/ArticleDto.java
@@ -0,0 +1,15 @@
+package net.newsportal.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.newsportal.models.Source;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ArticleDto {
+    private String title;
+    private String body;
+    private Source source;
+}

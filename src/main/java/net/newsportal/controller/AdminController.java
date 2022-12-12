fe180744296b63fe19ae381b9d34efb4e3291950
hugescrub <hexi9001@gmail.com>
2022-12-12 20:21:14 +0300

    Cleanup, cors

--- src/main/java/net/newsportal/controller/AdminController.java
+++ src/main/java/net/newsportal/controller/AdminController.java
@@ -10,14 +10,13 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class AdminController {
 
     private final ArticleRepository articleRepository;

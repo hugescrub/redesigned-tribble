ba1bd82a04e7b811c4cea853ae11d3c791620103
Alexey <96534372+alexey7126@users.noreply.github.com>
2022-12-13 10:01:35 +0300

    Checking role and auth

--- src/main/java/net/newsportal/controller/HomeController.java
+++ src/main/java/net/newsportal/controller/HomeController.java
@@ -1,13 +1,20 @@
 package net.newsportal.controller;
 
 import net.newsportal.models.Article;
+import net.newsportal.models.ERole;
+import net.newsportal.models.Role;
 import net.newsportal.repository.ArticleRepository;
+import net.newsportal.repository.UserRepository;
+import net.newsportal.service.CookieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.util.WebUtils;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,14 +22,24 @@ import java.util.stream.Collectors;
 public class HomeController {
 
     private final ArticleRepository articleRepository;
+    private final UserRepository userRepository;
+    private final CookieService cookieService;
 
     @Autowired
-    public HomeController(ArticleRepository articleRepository) {
+    public HomeController(ArticleRepository articleRepository, UserRepository userRepository, CookieService cookieService) {
         this.articleRepository = articleRepository;
+        this.userRepository = userRepository;
+        this.cookieService = cookieService;
     }
 
     @GetMapping
-    public String getHome(Model model) {
+    public String getHome(Model model, HttpServletRequest request) {
+        String userRole = cookieService.getRoleFromCookie(request);
+        boolean isLoggedIn = false;
+        if (userRole != "") isLoggedIn = true;
+        model.addAttribute("role", userRole);
+        model.addAttribute("isLoggedIn", isLoggedIn);
+
         List<Article> articles = articleRepository.findAll()
                 .stream()
                 .filter(article -> article.isApproved() && article.getTag() != null)
@@ -32,12 +49,24 @@ public class HomeController {
     }
 
     @GetMapping("/about")
-    public String about() {
+    public String about(Model model, HttpServletRequest request) {
+        String userRole = cookieService.getRoleFromCookie(request);
+        boolean isLoggedIn = false;
+        if (userRole != "") isLoggedIn = true;
+        model.addAttribute("role", userRole);
+        model.addAttribute("isLoggedIn", isLoggedIn);
+
         return "about";
     }
 
     @GetMapping("/article/{id}")
-    public String getArticle(Model model, @PathVariable Long id) {
+    public String getArticle(Model model, @PathVariable Long id, HttpServletRequest request) {
+        String userRole = cookieService.getRoleFromCookie(request);
+        boolean isLoggedIn = false;
+        if (userRole != "") isLoggedIn = true;
+        model.addAttribute("role", userRole);
+        model.addAttribute("isLoggedIn", isLoggedIn);
+
         if (articleRepository.findById(id).isPresent()) {
             Article article = articleRepository.findById(id).get();
             if (article.isApproved() && article.getTag() != null)

367a26a6b9bfcf1eee89825442a07170030b180d
Alexey <96534372+alexey7126@users.noreply.github.com>
2022-12-13 10:01:05 +0300

    Get role from cookie

--- src/main/java/net/newsportal/service/CookieService.java
+++ src/main/java/net/newsportal/service/CookieService.java
@@ -1,9 +1,11 @@
 package net.newsportal.service;
 
+import net.newsportal.models.ERole;
 import net.newsportal.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.WebUtils;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -32,5 +34,22 @@ public class CookieService {
             return new Cookie("CkieUsrSessionID", userRepository.findByUsername(username).getId().toString());
         else
             throw new UsernameNotFoundException("No user found with such username");
+    }
+
+    public String getRoleFromCookie(HttpServletRequest request) {
+        Cookie cookie = WebUtils.getCookie(request, "CkieUsrSessionID");
+        String userRole = "";
+        if (cookie != null) {
+            var user = userRepository.findById(Long.valueOf(cookie.getValue()));
+            var roles = user.get().getRoles();
+            for (var role : roles) {
+                if (role.getRole() == ERole.ROLE_ADMIN) {
+                    userRole = "admin";
+                } else if (role.getRole() == ERole.ROLE_CREATOR) {
+                    userRole = "creator";
+                }
+            }
+        }
+        return userRole;
     }
 }

d19179370f93446d7b88df312789f1cff394be9b
hugescrub <hexi9001@gmail.com>
2022-12-16 13:53:55 +0300

    Calling validation service, fixed null id being passed

--- src/main/java/net/newsportal/service/ArticleService.java
+++ src/main/java/net/newsportal/service/ArticleService.java
@@ -1,9 +1,8 @@
 package net.newsportal.service;
 
+import lombok.extern.slf4j.Slf4j;
 import net.newsportal.models.Article;
 import net.newsportal.repository.ArticleRepository;
-import net.newsportal.repository.SourceRepository;
-import net.newsportal.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -11,39 +10,32 @@ import org.springframework.web.reactive.function.client.WebClient;
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 
+@Slf4j
 @Service
 public class ArticleService {
 
-    private static final String BASE_URL = "http://serviceAddr:servicePort";
+    private static final String VALIDATION_BASE_URL = "http://localhost:8081";
 
     private final ArticleRepository articleRepository;
-    private final SourceRepository sourceRepository;
-    private final UserRepository userRepository;
 
     @Autowired
-    public ArticleService(ArticleRepository articleRepository,
-                          SourceRepository sourceRepository,
-                          UserRepository userRepository) {
+    public ArticleService(ArticleRepository articleRepository) {
         this.articleRepository = articleRepository;
-        this.sourceRepository = sourceRepository;
-        this.userRepository = userRepository;
     }
 
     @Transactional
     public boolean createArticle(String title, String body) {
         if (!articleRepository.existsByTitle(title)) {
             Article article = new Article(title, body, LocalDateTime.now());
-            Long articleId = article.getId();
             articleRepository.save(article);
 
-            WebClient client = WebClient.create(BASE_URL);
+            WebClient client = WebClient.create(VALIDATION_BASE_URL);
             client.post()
-                    .uri("/serviceValidationEndpoint")
-                    .bodyValue(articleId)
+                    .uri("/api/validator/validate")
+                    .bodyValue(articleRepository.findByTitle(title).getId())
                     .retrieve()
                     .bodyToMono(String.class)
                     .subscribe(System.out::println);
-
             return true;
         }
         return false;

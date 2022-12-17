8c53543dbaa48046565f8cf7975237ebcb37213e
hugescrub <hexi9001@gmail.com>
2022-12-18 01:11:11 +0300

    Sending cookie to validation service

--- src/main/java/net/newsportal/service/ArticleService.java
+++ src/main/java/net/newsportal/service/ArticleService.java
@@ -3,12 +3,17 @@ package net.newsportal.service;
 import lombok.extern.slf4j.Slf4j;
 import net.newsportal.models.Article;
 import net.newsportal.repository.ArticleRepository;
+import net.newsportal.security.CookiesFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 @Slf4j
 @Service
@@ -24,8 +29,15 @@ public class ArticleService {
     }
 
     @Transactional
-    public boolean createArticle(String title, String body) {
+    public boolean createArticle(String title, String body, HttpServletRequest request) {
         if (!articleRepository.existsByTitle(title)) {
+            Assert.notNull(request, "Request was null.");
+
+            Cookie c = Arrays.stream(request.getCookies())
+                    .filter(cookie -> cookie.getName().equals(CookiesFilter.COOKIE_NAME))
+                    .findFirst()
+                    .get();
+
             Article article = new Article(title, body, LocalDateTime.now());
             articleRepository.save(article);
 
@@ -33,6 +45,7 @@ public class ArticleService {
             client.post()
                     .uri("/api/validator/validate")
                     .bodyValue(articleRepository.findByTitle(title).getId())
+                    .headers(httpHeaders -> httpHeaders.add(c.getName(), c.getValue()))
                     .retrieve()
                     .bodyToMono(String.class)
                     .subscribe(System.out::println);
